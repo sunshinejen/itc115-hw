4cbe540b881be5e21c55b6ced2d9b32c7fcae8c0
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2018-11-14 15:23:42 -0800

    isSorted assignment

---
+++ assignment-6/Sorted.java
@@ -0,0 +1,4 @@
+//Write a method called isSorted that accepts an array of real numbers as a parameter and returns true if the list is sorted (nondecreasing) 
+//order and false otherwise. For example, if arrays list1 and list2 store [16.1, 12.3, 22.2, 14.4] and [1.5, 4.3, 7.0, 19.5, 25.1, 46.2]
+//respectively the calls isSorted(list1) and isSorted(list2) and should return false and true respectively. Assume the array has at least
+//one element. A one-element array is considered sorted.
