ecc55a5908c5ea28a20ae982a68bcf8be634c4af
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2018-11-28 15:26:54 -0800

    assignment 7

---
+++ assignment7/product.java
@@ -0,0 +1,9 @@
+//The product data should include a product code, description, price and product count. 
+//In addition to the getter and setter method, the class should implement toString and also a formatted output of the price (Hint: use the NumberFormat class. Here is the implementation of your formattedPrice method: 
+ //public String getPriceFormatted() 
+   // {
+     //   String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
+       // return formattedPrice;
+    //}
+//From the client app create instance of 2-3 different products and use the toString method to display their data.
+//Optional: Keep track of the count of the products created and call the getCount method to verify that it is correct. (Where could you keep track of the count of products?)
